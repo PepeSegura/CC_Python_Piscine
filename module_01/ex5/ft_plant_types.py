4272fe10a5e7daf62a136e12131a525b58daf3ca
Pepe Segura <psegura-@student.42madrid.com>
2025-12-30 19:50:02 +0100

    mixing ex6

--- module_01/ex5/ft_plant_types.py
+++ module_01/ex5/ft_plant_types.py
@@ -7,7 +7,7 @@ class Plant:
         self._age = age
 
     def __str__(self):
-        return f"{self._name}: {self._height}cm, {self._age} days old"
+        return f"{self._name} ({type(self).__name__}): {self._height}cm, {self._age} days"
 
 
 class Flower(Plant):
@@ -16,10 +16,10 @@ class Flower(Plant):
         self._color = color
 
     def __str__(self):
-        return f"{super().__str__()} - Color {self._color}"
+        return f"{super().__str__()}, {self._color} color"
 
     def bloom(self):
-        print(f"The {self._name} is blooming <3")
+        print(f"{self._name} is blooming beautifully!")
 
 
 class Tree(Plant):
@@ -28,10 +28,10 @@ class Tree(Plant):
         self._trunk_diameter = trunk_diameter
 
     def __str__(self):
-        return f"{super().__str__()} - Trunk diameter: {self._trunk_diameter}"
+        return f"{super().__str__()}, {self._trunk_diameter}cm diameter"
 
     def produce_shade(self):
-        print(f"The {self._name} is casting a lot of shade!!")
+        print(f"{self._name} is casting a lot of shade!!")
 
 
 class Vegetable(Plant):
@@ -46,16 +46,29 @@ class Vegetable(Plant):
 - Season: {self._harvest_season} - {self._nutritional_value}"
 
 
-plant = Plant("Rose", 10, 5)
-print(plant)
+print("=== Garden Plant Types ===\n")
 
 flower = Flower("Rose", 10, 10, "red")
 print(flower)
 flower.bloom()
 
+flower2 = Flower("Rose2", 15, 5, "blue")
+print(flower2)
+flower2.bloom()
+print()
+
 tree = Tree("Pine", 250, 365, 50)
 print(tree)
 tree.produce_shade()
 
+tree2 = Tree("Pine2", 10, 5, 20)
+print(tree2)
+tree2.produce_shade()
+print()
+
+
 vegetable = Vegetable("Tomato", 80, 90, "summer", "vitamin C")
 print(vegetable)
+
+vegetable2 = Vegetable("Onion", 32, 10, "winter", "vitamin B6")
+print(vegetable2)
